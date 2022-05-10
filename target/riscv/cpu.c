87ef75f9a7b0388552a1f8c3da2c202821512f4a
Peer Adelt <peer.adelt@hni.uni-paderborn.de>
2022-05-10 11:59:19 +0200

    disabled cpu register reset, disabled misa logging

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -129,38 +129,38 @@ static void set_misa(CPURISCVState *env, RISCVMXL mxl, uint32_t ext)
     env->misa_ext_mask = env->misa_ext = ext;
 
 #ifdef CONFIG_FEAR5
-    if (qemu_loglevel_mask(FEAR5_LOG_GOLDENRUN)) {
-        FILE *fd = qemu_log_lock();
-        qemu_log("----------------\n");
-        qemu_log("MISA: ");
+    // if (qemu_loglevel_mask(FEAR5_LOG_GOLDENRUN)) {
+    //     FILE *fd = qemu_log_lock();
+    //     qemu_log("----------------\n");
+    //     qemu_log("MISA: ");
 
-        /* RXLEN */
-        int rxlen = -1;
-        if (mxl == MXL_RV32) {
-            rxlen = 32;
-        }
-        if (mxl == MXL_RV64) {
-            rxlen = 64;
-        }
+    //     /* RXLEN */
+    //     int rxlen = -1;
+    //     if (mxl == MXL_RV32) {
+    //         rxlen = 32;
+    //     }
+    //     if (mxl == MXL_RV64) {
+    //         rxlen = 64;
+    //     }
 
-        /* Modules */
-        if (ext & RVI) qemu_log("rv%di", rxlen);
-        if (ext & RVE) qemu_log("rv%de", rxlen);
-        if (ext & RVM) qemu_log(",rv%dm", rxlen);
-        if (ext & RVA) qemu_log(",rv%da", rxlen);
-        if (ext & RVF) qemu_log(",rv%df", rxlen);
-        if (ext & RVD) qemu_log(",rv%dd", rxlen);
-        if (ext & RVC) {
-            qemu_log(",rv%dc", rxlen);
-            if (ext & RVF) qemu_log(",rv%dfc", rxlen);
-            if (ext & RVD) qemu_log(",rv%ddc", rxlen);
-        }
-        if (ext & RVS) qemu_log(",rv%ds", rxlen);
-        if (ext & RVU) qemu_log(",rv%du", rxlen);
+    //     /* Modules */
+    //     if (ext & RVI) qemu_log("rv%di", rxlen);
+    //     if (ext & RVE) qemu_log("rv%de", rxlen);
+    //     if (ext & RVM) qemu_log(",rv%dm", rxlen);
+    //     if (ext & RVA) qemu_log(",rv%da", rxlen);
+    //     if (ext & RVF) qemu_log(",rv%df", rxlen);
+    //     if (ext & RVD) qemu_log(",rv%dd", rxlen);
+    //     if (ext & RVC) {
+    //         qemu_log(",rv%dc", rxlen);
+    //         if (ext & RVF) qemu_log(",rv%dfc", rxlen);
+    //         if (ext & RVD) qemu_log(",rv%ddc", rxlen);
+    //     }
+    //     if (ext & RVS) qemu_log(",rv%ds", rxlen);
+    //     if (ext & RVU) qemu_log(",rv%du", rxlen);
 
-        qemu_log("\n\n");
-        qemu_log_unlock(fd);
-    }
+    //     qemu_log("\n\n");
+    //     qemu_log_unlock(fd);
+    // }
 #endif
 }
 
@@ -491,13 +491,13 @@ static void riscv_cpu_reset(DeviceState *dev)
 #endif
 
 #ifdef CONFIG_FEAR5
-    memset(env->gpr, 0, sizeof(uint32_t) * 32);
-    memset(env->fpr, 0, sizeof(uint64_t) * 32);
+    // memset(env->gpr, 0, sizeof(uint32_t) * 32);
+    // memset(env->fpr, 0, sizeof(uint64_t) * 32);
 
-    for (int i=0; i<4096; i++) {
-        riscv_csr_write(env, i, 0);
-    }
-    riscv_csr_write(env, 0x301, 0x40001105); // MISA Register
+    // for (int i=0; i<4096; i++) {
+    //     riscv_csr_write(env, i, 0);
+    // }
+    // riscv_csr_write(env, 0x301, 0x40001105); // MISA Register
 #endif
 }
 
